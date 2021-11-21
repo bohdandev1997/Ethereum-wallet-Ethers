a5bacf323aafb3d7ed74dbeb4e84bc6a9d327e22
unknown <bbohdan1997@gmail.com>
2021-11-21 17:07:10 +0200

    fixed eslint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
